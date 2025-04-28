9156d7a439374bcbd23eb1cfb9c53fa07509e5fa
Nasrin-F <rinfa991@gmail.com>
2025-04-28 00:39:53 -0400

    Updated login logout register pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,26 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Import your components
+import LoginPanel from './components/Login/Login';  // Your Login component
+import RegisterPanel from './components/Register/Register';  // Your Register component
+import Header from './components/Header/Header';  // Your Home component
+
 
 function App() {
   return (
     <Routes>
+      {/* Header Route */}
+      <Route path="/" element={<Header />} />
+      
+      {/* Login Page Route */}
       <Route path="/login" element={<LoginPanel />} />
+      
+      {/* Register Page Route */}
+      <Route path="/register" element={<RegisterPanel />} />
+      
     </Routes>
   );
 }
+
 export default App;
